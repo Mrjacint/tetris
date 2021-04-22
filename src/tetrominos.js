d11a03f4d7fbb7be7b0af7b33560f029da375300
Mrjacint <mrjacint@gmail.com>
2021-04-22 16:10:53 +0100

    Fix variable name

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,5 +1,5 @@
 export const TETROMINOS = {
-  0: { shape: [[0]], color: "0,0,0" },
+  0: { shape: [[0]], color: "0, 0, 0" },
   I: {
     shape: [
       [0, "I", 0, 0],
@@ -60,7 +60,7 @@ export const TETROMINOS = {
 
 export const randomTetromino = () => {
   const tetrominos = "IJLOSTZ";
-  const randomTetromino =
+  const randTetromino =
     tetrominos[Math.floor(Math.random() * tetrominos.length)];
-  return TETROMINOS[randomTetromino];
+  return TETROMINOS[randTetromino];
 };
