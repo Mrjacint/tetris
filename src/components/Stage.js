9367a2c7c687a4fc001322251cd7006fd2d8b1bf
Mrjacint <mrjacint@gmail.com>
2021-04-22 16:16:32 +0100

    Clean code

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,14 +2,10 @@ import { StyledStage } from "./styles/StyledStage";
 
 import Cell from "./Cell";
 
-const Stage = ({ stage }) => {
-  return (
-    <StyledStage width={stage[0].length} heigth={stage.length}>
-      {stage.map((row) =>
-        row.map((cell, i) => <Cell key={i} type={cell[0]} />)
-      )}
-    </StyledStage>
-  );
-};
+const Stage = ({ stage }) => (
+  <StyledStage width={stage[0].length} height={stage.length}>
+    {stage.map((row) => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+  </StyledStage>
+);
 
 export default Stage;
